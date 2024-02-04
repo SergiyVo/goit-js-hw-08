5ab2e733c1dd2af58d6410f485f1491fc6918891
unknown <voroshnin.s@gmail.com>
2024-02-05 00:33:32 +0200

    Start

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const userName = document.getElementById('name-input');
-const userText = document.getElementById('name-output');
-
-userName.addEventListener('input', () => {
-    const name = userName.value.trim();
-    userText.innerHTML = `${name}` || 'Anonymous';
-});
